704bfa8f525554e2c52662bad57367943d79de6e
mattlangsenkamp <langsenkamp.4@osu.edu>
2019-04-04 21:56:37 -0400

    initial skeleton

---
+++ augmentation_methods.py
@@ -0,0 +1,12 @@
+
+# returns augmented corpus of documents
+def method_1(corpus, word_vectors):
+    pass
+
+# returns augmented corpus of documents
+def method_2(corpus, word_vectors):
+    pass
+
+# returns augmented corpus of documents
+def method_3(corpus, word_vectors):
+    pass
