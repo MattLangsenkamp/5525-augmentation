0e452945c5b05c231b04e98ac1006754f5be88d6
mattlangsenkamp <langsenkamp.4@osu.edu>
2019-04-16 15:51:16 -0400

    small testing, fixed some bugs

--- augmentation_methods.py
+++ augmentation_methods.py
@@ -1,12 +1,76 @@
+import numpy as np
+import random as r
+import nltk
+from itertools import chain
 
 # returns augmented corpus of documents
 def method_1(corpus, y, word_vectors):
-    return None
+    augmented_data = []
+    augmented_y = []
+    for i in range(len(corpus)):
+        sample = corpus[i]
+        new_sample = sample.copy()
+        indices = pick_word_indices(sample)
+        for index in indices:
+            new_sample[index] = word_vectors.most_similar(sample[index])
+            
+        augmented_data.append(sample)
+        augmented_y.append(y[i])
+
+        if len(indices) > 0:
+            augmented_data.append(new_sample)
+            augmented_y.append(y[i])
+
+    return augmented_data, augmented_y
 
 # returns augmented corpus of documents
 def method_2(corpus, y, word_vectors):
-    return None
+    augmented_data = []
+    augmented_y = []
+    for i in range(len(corpus)):
+        sample = corpus[i].copy()
+        indices = pick_word_indices(sample)
+        for index in indices:
+            new_sample = list(chain(sample[:index], [word_vectors.most_similar(sample[index])], sample[index + 1:]))
+        augmented_data.append(new_sample)
+        augmented_y.append(y[i])
+
+    return augmented_data, augmented_y
+
 
 # returns augmented corpus of documents
 def method_3(corpus, y, word_vectors):
-    return None
+    augmented_data = []
+    augmented_y = []
+    for i in range(len(corpus)):
+        sample = corpus[i]
+        new_sample = sample.copy()
+        indices = pick_word_indices_verb_or_adjective(sample)
+        for index in indices:
+            new_sample[index] = word_vectors.most_similar(sample[index])
+        
+        augmented_data.append(sample)
+        augmented_y.append(y[i])
+
+        if len(indices) > 0:
+            augmented_data.append(new_sample)
+            augmented_y.append(y[i])
+
+    return augmented_data, augmented_y
+
+
+def pick_word_indices(sample):
+    #r.seed(1)
+    amount_words_swapped = r.randint(0, max(len(sample)-1, 0))
+    return sorted(r.sample(list(range(len(sample))), amount_words_swapped))
+
+def pick_word_indices_verb_or_adjective(sample):
+    
+    tags = nltk.pos_tag(sample)
+    valid_indices = []
+    for i in range(len(tags)):
+        if 'VB' in tags[i][1] or 'JJ' in tags[i][1]:
+            valid_indices.append(i)
+
+    amount_words_swapped = r.randint(0, max(len(valid_indices)-1, 0))
+    return sorted(r.sample(valid_indices, amount_words_swapped))
--- word_vectors.py
+++ word_vectors.py
@@ -1,7 +1,47 @@
 import gensim.models.word2vec as w2v
+import multiprocessing
 
 # return word gensim word2vec object. it has methods for obtaining 
 # the most similat word and the entire coprpus etc.
 def get_word_vectors(corpus):
-    # TODO 
-    return None
+
+    num_features = 300
+    min_word_count = 1
+    num_workers = multiprocessing.cpu_count()
+    context_size = 5
+    downsampling = 1e-5
+    seed = 1
+
+    vecs = w2v.Word2Vec(
+        sg=1,
+        seed=seed,
+        workers=num_workers,
+        size=num_features,
+        min_count=min_word_count,
+        window=context_size,
+        sample=downsampling
+    )
+
+    vecs.build_vocab(corpus)
+    vecs.train(corpus, total_examples = len(corpus), epochs = 100)
+
+    return vecs
+
+test_corpus = [
+        ['I', 'like', 'donuts','.'],
+        ['I', 'like', 'bagels','.'],
+        ['I', 'like', 'candy','.'],
+        ['I', 'like', 'bacon','.'],
+        ['I', 'like', 'salmon','.'],
+        ['I', 'love', 'donuts','.'],
+        ['I', 'love', 'bagels', '.'],
+        ['I', 'love', 'candy', '.'],
+        ['I', 'love', 'bacon', '.'],
+        ['I', 'love', 'salmon', '.'],
+        ['I', 'hate', 'broccoli', '.'],
+        ['broccoli', 'is', 'bad', '.'],
+        ['broccoli', 'is', 'not', 'cool', '.'],
+        ['broccoli', 'is', 'smelly', 'i', 'hate', 'it', '.']
+        ]
+
+wv = get_word_vectors(test_corpus)
