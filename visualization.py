540a72cb2128f333c069a6951b7b4411c5ffa522
mattlangsenkamp <langsenkamp.4@osu.edu>
2019-04-22 15:07:52 -0400

    all vis stuff added

--- visualization.py
+++ visualization.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter, MaxNLocator
+from sklearn.manifold import TSNE
+import pandas as pd
+import seaborn as sns
 
 def visualize_confusion_matrix(conf_mat, classes,
                           title=None,
@@ -44,7 +47,35 @@ def acc_bar_plot(acc_list, labels):
 def visualize_vectors(vectors):
     # use demensionality reduction to get vectors into two or three dimensions
     # save as png
-    pass
+    tsne = TSNE(n_components=2, random_state=0)
+    all_word_vectors_matrix = vectors.wv.syn0
+    all_word_vectors_matrix_2d = tsne.fit_transform(all_word_vectors_matrix)
+    
+    points = pd.DataFrame(
+        [
+            (word, coords[0], coords[1])
+            for word, coords in [
+                (word, all_word_vectors_matrix_2d[vectors.wv.vocab[word].index])
+                for word in vectors.wv.vocab
+            ]
+        ],
+        columns=["word", "x", "y"]
+    )
+    sns.set_context("poster")
+    points.plot.scatter("x", "y", s=10, figsize=(20, 12))
+    return points
+    
+def plot_region(x_bounds, y_bounds, points):
+    slice = points[
+        (x_bounds[0] <= points.x) &
+        (points.x <= x_bounds[1]) & 
+        (y_bounds[0] <= points.y) &
+        (points.y <= y_bounds[1])
+    ]
+    
+    ax = slice.plot.scatter("x", "y", s=35, figsize=(10, 8))
+    for i, point in slice.iterrows():
+        ax.text(point.x + 0.005, point.y + 0.005, point.word, fontsize=11)
 
 def format_fn(tick_val, tick_pos):
     labels = ['Original', 'Method 1', 'Method 2', 'Method 3']
